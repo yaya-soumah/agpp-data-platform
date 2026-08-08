f1eb1baed6a35a052b76ea6e48041721856406bd
yaya-soumah <yaya-soumah@outlook.com>
2026-08-08 19:06:37 +0800

    feat: add exception package API

--- src/shared/exceptions/__init__.py
+++ src/shared/exceptions/__init__.py
@@ -1,3 +1,17 @@
 from .base import AGPPException
+from .transformation import TransformationError
+from .validation import ValidationError 
+from .infrastructure import InfrastructureError
+from .configuration import ConfigurationError
+from .extraction import ExtractionError
+from .loading import LoadingError
 
-__all__ = ["AGPPException"]
+__all__ = [
+    "AGPPException",
+    "TransformationError",
+    "ValidationError",
+    "InfrastructureError",
+    "ConfigurationError",
+    "ExtractionError",
+    "LoadingError"
+]
